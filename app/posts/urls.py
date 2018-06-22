ff15c4c97a685e6a79b59555edf60ef1893f415b
chrisaor <pchpch0070@gmail.com>
2018-06-22 12:39:36 +0900

    post_list, post_detail view와 templates 연결

---
+++ app/posts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from posts import views
+
+urlpatterns = [
+    path('', views.post_list, name='post-list'),
+    path('<int:pk>/', views.post_detail, name='post-detail'),
+
+]
--- app/posts/views.py
+++ app/posts/views.py
@@ -1,2 +1,18 @@
 from django.shortcuts import render
 
+from posts.models import Post
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    context = {
+        'posts':posts,
+    }
+    return render(request, 'posts/post_list.html', context)
+
+def post_detail(request, pk):
+    post = Post.objects.get(id=pk)
+    context = {
+        'post':post,
+    }
+    return render(request, 'posts/post_detail.html', context)
